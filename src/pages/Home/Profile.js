9cd37c94f10a79b3b2385263cc8e7b66d1bb03b3
KnowsCount <knowscount@gmail.com>
2021-05-12 10:31:51 +0800

    chore: change misc stuff

--- src/pages/Home/Profile.js
+++ src/pages/Home/Profile.js
@@ -18,97 +18,115 @@ import Text from 'components/Text';
 import './Profile.css';
 
 const ProfileText = ({ status, titleId }) => (
-  <Fragment>
-    <Heading
-      className={classNames('profile__title', `profile__title--${status}`)}
-      level={3}
-      id={titleId}
-    >
-      <DecoderText text="Hi there" start={status !== 'exited'} delay={500} />
-    </Heading>
-    <Text
-      className={classNames('profile__description', `profile__description--${status}`)}
-      size="l"
-    >
-      I’m Hamish, currently I live in Sydney working as a senior product designer at{' '}
-      <Link href="https://www.qwilr.com">Qwilr</Link>. My projects include UX design, UI
-      animations, and icon illustration. Being comfortable with code allows me to rapidly
-      prototype and validate experiences. If you're interested in the tools and software I
-      use check out my <Link href="/uses">uses page</Link>.
-    </Text>
-    <Text
-      className={classNames('profile__description', `profile__description--${status}`)}
-      size="l"
-    >
-      In my spare time I like to practice Brazilian Jiu Jitsu, play video games, and{' '}
-      <Link href="/projects/volkihar-knight">make mods</Link>. I’m always down for hearing
-      about new projects, so feel free to drop me a line.
-    </Text>
-  </Fragment>
+    <Fragment>
+        <Heading
+            className={classNames('profile__title', `profile__title--${status}`)}
+            level={3}
+            id={titleId}
+        >
+            <DecoderText text="About TEDx" start={status !== 'exited'} delay={500} />
+        </Heading>
+        <Text
+            className={classNames(
+                'profile__description',
+                `profile__description--${status}`
+            )}
+            size="l"
+        >
+            <Link href="https://www.ted.com/about/programs-initiatives/tedx-program">
+                TEDx
+            </Link>{' '}
+            is a grassroots initiative, created in the spirit of TED’s overall mission to
+            research and discover “ideas worth spreading.” TEDx brings the spirit of TED
+            to local communities around the globe through TEDx events. These events are
+            organized by passionate individuals who seek to uncover new ideas and to share
+            the latest research in their local areas that spark conversations in their
+            communities. TEDx events include live speakers and recorded TED Talks, and are
+            organized independently under a free license granted by TED.
+        </Text>
+        <Text
+            className={classNames(
+                'profile__description',
+                `profile__description--${status}`
+            )}
+            size="l"
+        >
+            These events are not controlled by TED, but event organizers agree to abide by
+            our format, and are offered guidelines for curation, speaker coaching, event
+            organizing and more. They learn from us and from each other. More than 3000
+            events are now held annually.
+        </Text>
+    </Fragment>
 );
 
 const Profile = ({ id, visible, sectionRef }) => {
-  const titleId = `${id}-title`;
+    const titleId = `${id}-title`;
 
-  return (
-    <Section
-      className="profile"
-      as="section"
-      id={id}
-      ref={sectionRef}
-      aria-labelledby={titleId}
-      tabIndex={-1}
-    >
-      <Transition in={visible} timeout={0} onEnter={reflow}>
-        {status => (
-          <div className="profile__content">
-            <div className="profile__column">
-              <ProfileText status={status} titleId={titleId} />
-              <Button
-                secondary
-                className={classNames('profile__button', `profile__button--${status}`)}
-                href="/contact"
-                icon="send"
-              >
-                Send me a message
-              </Button>
-            </div>
-            <div className="profile__column">
-              <div className="profile__tag" aria-hidden>
-                <Divider
-                  notchWidth="64px"
-                  notchHeight="8px"
-                  collapsed={status !== 'entered'}
-                  collapseDelay={1000}
-                />
-                <div
-                  className={classNames(
-                    'profile__tag-text',
-                    `profile__tag-text--${status}`
-                  )}
-                >
-                  About Me
-                </div>
-              </div>
-              <div className="profile__image-wrapper">
-                <Image
-                  reveal
-                  delay={100}
-                  placeholder={ProfileImgPlaceholder}
-                  srcSet={`${ProfileImg} 480w, ${ProfileImgLarge} 960w`}
-                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
-                  alt="Me standing in front of the Torii on Miyajima, an island off the coast of Hiroshima in Japan"
-                />
-                <KatakanaProfile
-                  className={classNames('profile__svg', `profile__svg--${status}`)}
-                />
-              </div>
-            </div>
-          </div>
-        )}
-      </Transition>
-    </Section>
-  );
+    return (
+        <Section
+            className="profile"
+            as="section"
+            id={id}
+            ref={sectionRef}
+            aria-labelledby={titleId}
+            tabIndex={-1}
+        >
+            <Transition in={visible} timeout={0} onEnter={reflow}>
+                {status => (
+                    <div className="profile__content">
+                        <div className="profile__column">
+                            <ProfileText status={status} titleId={titleId} />
+                            <Button
+                                secondary
+                                className={classNames(
+                                    'profile__button',
+                                    `profile__button--${status}`
+                                )}
+                                href="/contact"
+                                icon="send"
+                            >
+                                Send me a message
+                            </Button>
+                        </div>
+                        <div className="profile__column">
+                            <div className="profile__tag" aria-hidden>
+                                <Divider
+                                    notchWidth="64px"
+                                    notchHeight="8px"
+                                    collapsed={status !== 'entered'}
+                                    collapseDelay={1000}
+                                />
+                                <div
+                                    className={classNames(
+                                        'profile__tag-text',
+                                        `profile__tag-text--${status}`
+                                    )}
+                                >
+                                    About Me
+                                </div>
+                            </div>
+                            <div className="profile__image-wrapper">
+                                <Image
+                                    reveal
+                                    delay={100}
+                                    placeholder={ProfileImgPlaceholder}
+                                    srcSet={`${ProfileImg} 480w, ${ProfileImgLarge} 960w`}
+                                    sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                                    alt="Me standing in front of the Torii on Miyajima, an island off the coast of Hiroshima in Japan"
+                                />
+                                <KatakanaProfile
+                                    className={classNames(
+                                        'profile__svg',
+                                        `profile__svg--${status}`
+                                    )}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </Transition>
+        </Section>
+    );
 };
 
 export default Profile;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,18 +6,18 @@ import Profile from 'pages/Home/Profile';
 import Footer from 'components/Footer';
 import { usePrefersReducedMotion, useRouteTransition } from 'hooks';
 import { useLocation } from 'react-router-dom';
-import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
-import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
-import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
-import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
-import gamestackTexture from 'assets/gamestack-login.jpg';
-import gamestackTextureLarge from 'assets/gamestack-login-large.jpg';
-import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
-import gamestackTexture2 from 'assets/gamestack-list.jpg';
-import gamestackTexture2Large from 'assets/gamestack-list-large.jpg';
-import sliceTexture from 'assets/slice-app.jpg';
-import sliceTextureLarge from 'assets/slice-app-large.jpg';
-import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
+// import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
+// import sprTexture from 'assets/spr-lesson-builder-dark.jpg';
+// import sprTextureLarge from 'assets/spr-lesson-builder-dark-large.jpg';
+// import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
+// import gamestackTexture from 'assets/gamestack-login.jpg';
+// import gamestackTextureLarge from 'assets/gamestack-login-large.jpg';
+// import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
+// import gamestackTexture2 from 'assets/gamestack-list.jpg';
+// import gamestackTexture2Large from 'assets/gamestack-list-large.jpg';
+// import sliceTexture from 'assets/slice-app.jpg';
+// import sliceTextureLarge from 'assets/slice-app-large.jpg';
+// import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
 import iphone11 from 'assets/iphone-11.glb';
 import macbookPro from 'assets/macbook-pro.glb';
 import './index.css';
@@ -161,18 +161,18 @@ const Home = () => {
                 sectionRef={projectOne}
                 visible={visibleSections.includes(projectOne.current)}
                 index={1}
-                title="Designing the future of education"
-                description="Designing a platform to help educators build better online courseware"
-                buttonText="View Project"
+                title="The What"
+                description="What we aim to cover in our TEDx programme"
+                buttonText="View More"
                 buttonLink="/projects/smart-sparrow"
                 model={{
                     type: 'laptop',
                     alt: 'Smart Sparrow lesson builder',
                     textures: [
                         {
-                            src: sprTexture,
-                            srcSet: `${sprTexture} 800w, ${sprTextureLarge} 1440w`,
-                            placeholder: sprTexturePlaceholder,
+                            // src: sprTexture,
+                            // srcSet: `${sprTexture} 800w, ${sprTextureLarge} 1440w`,
+                            // placeholder: sprTexturePlaceholder,
                         },
                     ],
                 }}
@@ -183,23 +183,23 @@ const Home = () => {
                 sectionRef={projectTwo}
                 visible={visibleSections.includes(projectTwo.current)}
                 index={2}
-                title="Video game progress tracking"
-                description="Design and development for a video game tracking app built in React Native"
-                buttonText="View Website"
+                title="Sustainable Development"
+                description="How we are to achieve sustainable development in this TEDx programme"
+                buttonText="View More"
                 buttonLink="https://gamestackapp.com"
                 model={{
                     type: 'phone',
                     alt: 'App login screen',
                     textures: [
                         {
-                            src: gamestackTexture,
-                            srcSet: `${gamestackTexture} 254w, ${gamestackTextureLarge} 508w`,
-                            placeholder: gamestackTexturePlaceholder,
+                            // src: gamestackTexture,
+                            // srcSet: `${gamestackTexture} 254w, ${gamestackTextureLarge} 508w`,
+                            // placeholder: gamestackTexturePlaceholder,
                         },
                         {
-                            src: gamestackTexture2,
-                            srcSet: `${gamestackTexture2} 254w, ${gamestackTexture2Large} 508w`,
-                            placeholder: gamestackTexture2Placeholder,
+                            // src: gamestackTexture2,
+                            // srcSet: `${gamestackTexture2} 254w, ${gamestackTexture2Large} 508w`,
+                            // placeholder: gamestackTexture2Placeholder,
                         },
                     ],
                 }}
@@ -209,8 +209,8 @@ const Home = () => {
                 sectionRef={projectThree}
                 visible={visibleSections.includes(projectThree.current)}
                 index={3}
-                title="Biomedical image collaboration"
-                description="Increasing the amount of collaboration in Slice, an app for biomedical imaging"
+                title="Buy Ticket"
+                description="Buy a ticket to join our programme"
                 buttonText="View Project"
                 buttonLink="/projects/slice"
                 model={{
@@ -218,9 +218,9 @@ const Home = () => {
                     alt: 'Annotating a biomedical image in the Slice app',
                     textures: [
                         {
-                            src: sliceTexture,
-                            srcSet: `${sliceTexture} 980w, ${sliceTextureLarge} 1376w`,
-                            placeholder: sliceTexturePlaceholder,
+                            // src: sliceTexture,
+                            // srcSet: `${sliceTexture} 980w, ${sliceTextureLarge} 1376w`,
+                            // placeholder: sliceTexturePlaceholder,
                         },
                     ],
                 }}
