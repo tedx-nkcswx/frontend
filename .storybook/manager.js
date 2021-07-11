8f8b7a380b13a0ecbaf72becbe25f02a4a528edc
KnowsCount <knowscount@gmail.com>
2021-07-11 04:17:17 +0000

    fix: document does not have a valid `rel=canonical`

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,10 +2,11 @@ import { themes } from '@storybook/theming';
 import { addons } from '@storybook/addons';
 
 addons.setConfig({
-  theme: {
-    ...themes.dark,
-    brandImage: 'https://hamishw.com/icon.svg',
-    brandTitle: 'TEDx@NKCSWX team Components',
-    brandUrl: 'https://tedx.workermen.cyou',
-  },
+    theme: {
+        ...themes.dark,
+        // brandImage:
+        //     'https://knowscount-1304485449.cos.ap-shanghai.myqcloud.com/img/icon.svg',
+        brandTitle: 'TEDx@NKCSWX team Components',
+        brandUrl: 'https://tedx.workermen.cyou/',
+    },
 });
